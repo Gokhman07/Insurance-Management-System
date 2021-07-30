939466d48f996f1adedb9e404e0365f514df2a78
Ruslan Gokhman <61827720+Gokhman07@users.noreply.github.com>
2021-07-30 09:22:20 +0300

    Update index.js

--- src/database/index.js
+++ src/database/index.js
@@ -3,7 +3,7 @@ const { Sequelize } = require("sequelize");
 //logging - отключение mysql операции в консоли у библиотеки sequilize.
 // Чтобы включить нужно поставить значение, значение true
 
-const sequelize = new Sequelize("um3aa3gwvmfrkrur", "p9cuuw3xjofmp9gr", "b1hpfs4r2awoy6bc", {
+const sequelize = new Sequelize("kzomzh2bqwykzzdr", "bxe299i2vt7d53jx", "wfbes8j9zwstguzs", {
   host: "un0jueuv2mam78uv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   dialect: "mysql",
   define: {
